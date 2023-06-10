30105c377db180a3fdb2524386ee6925ae38aa45
Guillaume Aveline <guillaume@codr.fr>
2023-06-11 00:49:03 +0200

    Ex1 : add countChar()

--- src/ex1/index.test.ts
+++ src/ex1/index.test.ts
@@ -1,7 +1,11 @@
-import { sum } from '.'
+import { sum, countChars } from '.'
 
-describe('sum module', () => {
+describe('Ex1 : primitives', () => {
   test('adds 3 + 2 to equal 5', () => {
     expect(sum(3, 2)).toBe(5);
+  });
+
+  test('countChars should return 12 for "Hello World!"', () => {
+    expect(countChars("Hello World!")).toBe(12);
   });
 });
--- src/ex1/index.ts
+++ src/ex1/index.ts
@@ -1,1 +1,3 @@
 export const sum = (a, b) => a + b
+
+export const countChars = (str) => str.length
